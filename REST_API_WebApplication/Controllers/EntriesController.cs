707fcfe067eb64ea2eb2f530739a6ab4983e6bbf
mweihmann <m@weihmann.at>
2022-08-23 15:54:34 +0200

    Add project files.

---
+++ REST_API_WebApplication/Controllers/EntriesController.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using REST_API_WebApplication.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace REST_API_WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntriesController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public EntriesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Get methode
+        [HttpGet]
+        public JsonResult Get()
+        {
+            //select statement in query variable
+            string query = @"
+                            select Id, Description from
+                            dbo.Entry";
+
+            // table verweist auf den memory table
+            DataTable table = new DataTable();
+
+            // connectionstring in appsettings.json wird in sqldatasource variable geschrieben
+            string sqlDataSource = _configuration.GetConnectionString("RestAPIDatabase");
+
+            // reader um rows einer sqldb zu lesen
+            SqlDataReader myReader;
+
+            using(SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                // öffnen der verbindung zur sql db
+                myConn.Open();
+
+                // sql statement von query + connection werden gegen die sql db ausgeführt bzw in die variable cmd geschrieben
+                using (SqlCommand cmd = new SqlCommand(query, myConn))
+                {
+                    // sql statement + connection werden mit der executereader methode auf die variable myreader verwiesen.
+                    myReader = cmd.ExecuteReader();
+                    // in memory table läd den inhalt von myreader
+                    table.Load(myReader);
+
+                    //reader und verbindung schließen
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            // returned jsonresult der variable table
+            return new JsonResult(table);
+        }
+
+
+        [HttpGet("{id}")]
+        public JsonResult GetById(int id)
+        {
+            //select statement in query variable
+            string query = @"
+                            select * from dbo.Entry
+                            where Id=@Id";
+
+            // table verweist auf den memory table
+            DataTable table = new DataTable();
+
+            // connectionstring in appsettings.json wird in sqldatasource variable geschrieben
+            string sqlDataSource = _configuration.GetConnectionString("RestAPIDatabase");
+
+            // reader um rows einer sqldb zu lesen
+            SqlDataReader myReader;
+
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                // öffnen der verbindung zur sql db
+                myConn.Open();
+
+                // sql statement von query + connection werden in die variable cmd geschrieben
+                using (SqlCommand cmd = new SqlCommand(query, myConn))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+
+                    // sql statement + connection werden mit der executereader methode auf die variable myreader verwiesen.
+                    myReader = cmd.ExecuteReader();
+                    // in memory table läd den inhalt von myreader
+                    table.Load(myReader);
+
+                    //reader und verbindung schließen
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            // returned jsonresult der variable table
+            return new JsonResult(table);
+        }
+
+
+
+        [HttpPost]
+        public JsonResult Post(Entry entry)
+        {
+            //select statement in query variable
+            string query = @"
+                            insert into dbo.Entry
+                            values (@Description)";
+
+            // table verweist auf den memory table
+            DataTable table = new DataTable();
+
+            // connectionstring in appsettings.json wird in sqldatasource variable geschrieben
+            string sqlDataSource = _configuration.GetConnectionString("RestAPIDatabase");
+
+            // reader um rows einer sqldb zu lesen
+            SqlDataReader myReader;
+
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                // öffnen der verbindung zur sql db
+                myConn.Open();
+
+                // sql statement von query + connection werden in die variable cmd geschrieben
+                using (SqlCommand cmd = new SqlCommand(query, myConn))
+                {
+                    //sql statement von query + connection werden gegen die sql db ausgeführt mit den parametern um in description zu schreiben
+                    cmd.Parameters.AddWithValue("@Description", entry.Description);
+                    // sql statement + connection werden mit der executereader methode auf die variable myreader verwiesen.
+                    myReader = cmd.ExecuteReader();
+                    // in memory table läd den inhalt von myreader
+                    table.Load(myReader);
+
+                    //reader und verbindung schließen
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            // returned jsonresult der variable table
+            return new JsonResult("Added successful");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Entry entry)
+        {
+            //select statement in query variable
+            string query = @"
+                            update dbo.Entry
+                            set Description= @Description
+                            where Id=@Id";
+
+            // table verweist auf den memory table
+            DataTable table = new DataTable();
+
+            // connectionstring in appsettings.json wird in sqldatasource variable geschrieben
+            string sqlDataSource = _configuration.GetConnectionString("RestAPIDatabase");
+
+            // reader um rows einer sqldb zu lesen
+            SqlDataReader myReader;
+
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                // öffnen der verbindung zur sql db
+                myConn.Open();
+
+                // sql statement von query + connection werden in die variable cmd geschrieben
+                using (SqlCommand cmd = new SqlCommand(query, myConn))
+                {
+                    cmd.Parameters.AddWithValue("@Id", entry.Id);
+                    //sql statement von query + connection werden gegen die sql db ausgeführt mit den parametern um in description zu schreiben
+                    cmd.Parameters.AddWithValue("@Description", entry.Description);
+                    // sql statement + connection werden mit der executereader methode auf die variable myreader verwiesen.
+                    myReader = cmd.ExecuteReader();
+                    // in memory table läd den inhalt von myreader
+                    table.Load(myReader);
+
+                    //reader und verbindung schließen
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            // returned jsonresult der variable table
+            return new JsonResult("Updated successful");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            //select statement in query variable
+            string query = @"
+                            delete from dbo.Entry
+                            where Id=@Id";
+
+            // table verweist auf den memory table
+            DataTable table = new DataTable();
+
+            // connectionstring in appsettings.json wird in sqldatasource variable geschrieben
+            string sqlDataSource = _configuration.GetConnectionString("RestAPIDatabase");
+
+            // reader um rows einer sqldb zu lesen
+            SqlDataReader myReader;
+
+            using (SqlConnection myConn = new SqlConnection(sqlDataSource))
+            {
+                // öffnen der verbindung zur sql db
+                myConn.Open();
+
+                // sql statement von query + connection werden in die variable cmd geschrieben
+                using (SqlCommand cmd = new SqlCommand(query, myConn))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    
+                    // sql statement + connection werden mit der executereader methode auf die variable myreader verwiesen.
+                    myReader = cmd.ExecuteReader();
+                    // in memory table läd den inhalt von myreader
+                    table.Load(myReader);
+
+                    //reader und verbindung schließen
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            // returned jsonresult der variable table
+            return new JsonResult("Deleted successful");
+        }
+
+    }
+}
---
+++ REST_API_WebApplication/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+
+namespace REST_API_WebApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Enable CORS(cross origin requests) - allows the server to relax same origin policy
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            //JSON Serializer - convertig JSON to .NET object and back again by mapping .net object property names to json
+            services.AddControllersWithViews().AddNewtonsoftJson(options=>options.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options=>options.SerializerSettings.ContractResolver
+                =new DefaultContractResolver());
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //Enable CORS(cross origin requests) - allows the server to relax same origin policy
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
